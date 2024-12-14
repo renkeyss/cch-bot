02f62db193a4af3d7d700764e36efbaffd999a6c
renkeyss <renkeyss@gmail.com>
2024-12-14 14:35:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ import logging
 from openai import OpenAIError
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__main__)
+logger = logging.getLogger(__name__)
 
 _ = load_dotenv(find_dotenv())
 
