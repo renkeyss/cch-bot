f3d598e69611d16640f7a60a31ce60cd3db6e083
renkeyss <renkeyss@gmail.com>
2024-12-14 13:38:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ async def call_openai_assistant_api(user_message):
 
     except Exception as e:
         logger.error(f"調用 OpenAI 助手時出現未知錯誤: {e}")
-        return "系統出現錯誤，請稍後再試。"
+        return "系統出現錯誤，請稍後再試-9。"
 
 channel_secret = os.getenv('ChannelSecret', None)
 channel_access_token = os.getenv('ChannelAccessToken', None)
